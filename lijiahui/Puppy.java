6c25ce0fd3680d69cb7ca009ad851f8be4f12160
lijiahui <893087206@qq.com>
2018-01-26 15:57:12 +0800

    first commit

---
+++ lijiahui/Puppy.java
@@ -0,0 +1,11 @@
+public class Puppy{
+  public Puppy(String name)
+  {
+     System.out.println("ÃÉ¼áµÄÃû×Ö£º"+name);
+   }
+  public  static void main(String []args)
+  {
+     Puppy myPuppy = new Puppy("tommy");
+   }
+
+}
